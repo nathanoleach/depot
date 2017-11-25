deb816c21568c6bdaa680d13630eaa9dbb4d9d77
Nathan Leach <nathanoleach@gmail.com>
2017-11-25 00:54:12 -0500

    action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_cable.disable_request_forgery_protection = true
+
 end
